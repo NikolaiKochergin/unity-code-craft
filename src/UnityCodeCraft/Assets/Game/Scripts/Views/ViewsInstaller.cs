090fa4453ad44f140555772df1bd29b6cfd5e0a4
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-03-23 21:22:22 +0300

    удалил не используемую ссылку

--- src/UnityCodeCraft/Assets/Game/Scripts/Views/ViewsInstaller.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/Views/ViewsInstaller.cs
@@ -6,7 +6,6 @@ namespace Game.Views
     public sealed class ViewsInstaller : MonoInstaller
     {
         [SerializeField] private CoinParticleView _coinParticle;
-        [SerializeField] private PlanetPopupView _planetPopup;
         
         public override void InstallBindings()
         {
