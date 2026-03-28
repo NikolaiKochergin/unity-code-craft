f22254a430d8cf5f42d6d76e47b790337314a1b1
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-03-28 11:42:05 +0300

    вынет анимации money view в отдельный компонент money panel

---
+++ src/UnityCodeCraft/Assets/Game/Scripts/Views/Money/MoneyPanel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Views
+{
+    public class MoneyPanel : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _moneyText;
+        [SerializeField, Min(0)] private float _animationDuration = 0.75f;
+        
+        private readonly List<Sequence> _animationSequences = new();
+        private Coroutine _animationCoroutine;
+
+        public void SetText(float value, string format) => 
+            _moneyText.SetText(format, value);
+
+        public void AnimateIncrease(int previous, int current, string format)
+        {
+            StopAnimations();
+            _animationCoroutine = StartCoroutine(AddMoneyAnimation(previous, current - previous, format));
+            BounceAnimation();
+        }
+
+        public void AnimateSet(int current, string format)
+        {
+            StopAnimations();
+            _moneyText.SetText(format, current);
+            BounceAnimation();
+        }
+
+        private void BounceAnimation()
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            sequence
+                .AppendCallback(() => _animationSequences.Add(sequence))
+                .Append(_moneyText.transform.DOScale(new Vector3(1.1f, 1.1f, 1f), 0.2f))
+                .Append(_moneyText.transform.DOScale(Vector3.one, 0.4f))
+                .OnKill(() => _moneyText.transform.localScale = Vector3.one)
+                .OnComplete(() => _animationSequences.Remove(sequence));
+        }
+
+        private void StopAnimations()
+        {
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+                _animationCoroutine = null;
+            }
+            
+            foreach (Sequence sequence in _animationSequences) 
+                sequence.Kill();
+            
+            _animationSequences.Clear();
+        }
+        
+        private IEnumerator AddMoneyAnimation(float startMoney, float range, string format)
+        {
+            float progress = 0;
+
+            while (progress <= 1)
+            {
+                yield return null;
+
+                progress = Mathf.Min(1, progress + Time.deltaTime / _animationDuration);
+
+                float currentMoney = startMoney + range * progress;
+                _moneyText.SetText(format, currentMoney);
+            }
+            
+            _moneyText.SetText(format, startMoney + range);
+        }
+    }
+}
--- src/UnityCodeCraft/Assets/Game/Scripts/Views/Money/MoneyView.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/Views/Money/MoneyView.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
 using Game.Presenters;
 using R3;
 using TMPro;
@@ -12,14 +9,10 @@ namespace Game.Views
 {
     public class MoneyView : MonoBehaviour
     {
-        [SerializeField] private TMP_Text _moneyText;
-        [SerializeField, Min(0)] private float _animationDuration = 0.75f;
+        [SerializeField] private MoneyPanel _moneyPanel;
         
         private MoneyPresenter _presenter;
         
-        private readonly List<Sequence> _animationSequences = new();
-        private Coroutine _animationCoroutine;
-
         private IDisposable _disposables;
         
         [Inject]
@@ -28,7 +21,7 @@ namespace Game.Views
 
         private void OnEnable()
         {
-            _moneyText.SetText(_presenter.Format, _presenter.Money.CurrentValue);
+            _moneyPanel.SetText(_presenter.Money.CurrentValue, _presenter.Format);
             
             ReadOnlyReactiveProperty<int> money = _presenter.Money;
 
@@ -36,13 +29,13 @@ namespace Game.Views
             money
                 .Pairwise()
                 .Where(x => x.Current > x.Previous)
-                .Subscribe(OnMoneyEarned)
+                .Subscribe(x => _moneyPanel.AnimateIncrease(x.Previous, x.Current, _presenter.Format))
                 .AddTo(ref disposables);
             
             money
                 .Pairwise()
                 .Where(x => x.Previous > x.Current)
-                .Subscribe(OnMoneySpent)
+                .Subscribe(x => _moneyPanel.AnimateSet(x.Current, _presenter.Format))
                 .AddTo(ref disposables);
             
             _disposables = disposables.Build();
@@ -51,61 +44,6 @@ namespace Game.Views
         private void OnDisable() => 
             _disposables.Dispose();
 
-        private void OnMoneyEarned((int Previous, int Current) x)
-        {
-            StopAnimations();
-            _animationCoroutine = StartCoroutine(AddMoneyAnimation(x.Previous, x.Current - x.Previous));
-            BounceAnimation();
-        }
 
-        private void OnMoneySpent((int Previous, int Current) x)
-        {
-            StopAnimations();
-            _moneyText.SetText(_presenter.Format, x.Current);
-            BounceAnimation();
-        }
-
-        private void BounceAnimation()
-        {
-            Sequence sequence = DOTween.Sequence();
-
-            sequence
-                .AppendCallback(() => _animationSequences.Add(sequence))
-                .Append(_moneyText.transform.DOScale(new Vector3(1.1f, 1.1f, 1f), 0.2f))
-                .Append(_moneyText.transform.DOScale(Vector3.one, 0.4f))
-                .OnKill(() => _moneyText.transform.localScale = Vector3.one)
-                .OnComplete(() => _animationSequences.Remove(sequence));
-        }
-
-        private void StopAnimations()
-        {
-            if (_animationCoroutine != null)
-            {
-                StopCoroutine(_animationCoroutine);
-                _animationCoroutine = null;
-            }
-            
-            foreach (Sequence sequence in _animationSequences) 
-                sequence.Kill();
-            
-            _animationSequences.Clear();
-        }
-        
-        private IEnumerator AddMoneyAnimation(float startMoney, float range)
-        {
-            float progress = 0;
-
-            while (progress <= 1)
-            {
-                yield return null;
-
-                progress = Mathf.Min(1, progress + Time.deltaTime / _animationDuration);
-
-                float currentMoney = startMoney + range * progress;
-                _moneyText.SetText(_presenter.Format, currentMoney);
-            }
-            
-            _moneyText.SetText(_presenter.Format, startMoney + range);
-        }
     }
 }
