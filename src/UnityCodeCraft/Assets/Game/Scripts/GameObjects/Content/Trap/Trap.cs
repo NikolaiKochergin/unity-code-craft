91dec1252796e12ce2488407b6a453fdc8cfb698
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-04-12 16:58:28 +0300

    пофиксил и настроил ловушки

--- src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Trap/Trap.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Trap/Trap.cs
@@ -1,29 +1,46 @@
+using Game.Scripts.GameObjects.Components.Death;
 using UnityEngine;
 
 namespace Game.Scripts.GameObjects.Content.Trap
 {
-    public class Trap : MonoBehaviour
+    public class Trap : MonoBehaviour,
+        DeathHandleComponent.IAction
     {
         [SerializeField] private int _damage = 1;
         
         private CollisionComponent _collisionComponent;
+        private HealthComponent _healthComponent;
+        private DeathHandleComponent _deathHandleComponent;
 
         private void Awake()
         {
+            _healthComponent = GetComponent<HealthComponent>();
+            _deathHandleComponent = GetComponent<DeathHandleComponent>();
+            _deathHandleComponent.SetAction(this);
+
             _collisionComponent = GetComponent<CollisionComponent>();
 
             _collisionComponent.OnEntered += OnEntered;
         }
 
+        private void OnDestroy()
+        {
+            _collisionComponent.OnEntered -= OnEntered;
+        }
+
         private void OnEntered(Collision2D collision)
         {
-            if(!collision.otherCollider.TryGetComponent(out HealthComponent healthComponent))
+            if(!collision.collider.TryGetComponent(out HealthComponent healthComponent))
                 return;
-            
-            Debug.Log($"{name} collided with {collision.otherCollider.name}");
             
             healthComponent.TakeDamage(_damage);
             Destroy(gameObject);
+        }
+
+        void DeathHandleComponent.IAction.Invoke()
+        {
+            if(!_healthComponent.IsAlive)
+                Destroy(gameObject);
         }
     }
 }
