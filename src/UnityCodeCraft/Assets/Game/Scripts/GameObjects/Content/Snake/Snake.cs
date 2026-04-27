2f64f9b8969feb2ee795378fd5ad5be2612f5e84
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-04-28 00:13:13 +0300

    Заново добавил змею

--- src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Snake/Snake.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Snake/Snake.cs
@@ -2,8 +2,86 @@
 
 namespace Game
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(HealthComponent))]
+    [RequireComponent(typeof(ExtraGravityComponent))]
+    [RequireComponent(typeof(MoveComponent))]
+    [RequireComponent(typeof(LookComponent))]
+    [RequireComponent(typeof(CollisionComponent))]
     public class Snake : MonoBehaviour
     {
+        [SerializeField] private GameObject _tossAttack;
+        [SerializeField] private GameObject _target;
         
+        private Rigidbody2D _rigidbody;
+        private HealthComponent _healthComponent;
+        private ExtraGravityComponent _extraGravityComponent;
+        private MoveComponent _moveComponent;
+        private LookComponent _lookComponent;
+        private CollisionComponent _collisionComponent;
+        private FireComponent _tossComponent;
+        private TriggerComponent _characterTriggerComponent;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _healthComponent = GetComponent<HealthComponent>();
+            _extraGravityComponent = GetComponent<ExtraGravityComponent>();
+            _moveComponent = GetComponent<MoveComponent>();
+            _lookComponent = GetComponent<LookComponent>();
+            _collisionComponent = GetComponent<CollisionComponent>();
+            _tossComponent = _tossAttack.GetComponent<FireComponent>();
+            _characterTriggerComponent = GetComponentInChildren<TriggerComponent>();
+
+            _moveComponent.SetCondition(() => _healthComponent.IsAlive);
+            _tossComponent.SetCondition(() => _healthComponent.IsAlive);
+            
+            _healthComponent.OnDied += OnDied;
+            _collisionComponent.OnEntered += OnCollisionEntered;
+
+            _characterTriggerComponent.OnEntered += OnCharacterEntered;
+            _characterTriggerComponent.OnExited += OnCharacterExited;
+        }
+        
+        private void OnDestroy()
+        {
+            _healthComponent.OnDied -= OnDied;
+            _collisionComponent.OnEntered -= OnCollisionEntered;
+            _characterTriggerComponent.OnEntered -= OnCharacterEntered;
+            _characterTriggerComponent.OnExited -= OnCharacterExited;
+        }
+
+        private void OnCharacterEntered(Collider2D col)
+        {
+            if(col.gameObject.layer == LayerMask.NameToLayer(GameObjectTags.Character))
+                _target = col.gameObject;
+        }
+
+        private void OnCharacterExited(Collider2D col)
+        {
+            if(col.gameObject.layer == LayerMask.NameToLayer(GameObjectTags.Character))
+                _target = null;
+        }
+
+        private void Update()
+        {
+            _extraGravityComponent.enabled = _rigidbody.linearVelocityY < 0;
+
+            if (!_target) 
+                return;
+            
+            Vector2 direction = (_target.transform.position - transform.position).normalized;
+            _lookComponent.Look(_target.transform);
+            _moveComponent.Move(new Vector2(Mathf.Abs(direction.x), 0));
+        }
+
+        private void OnCollisionEntered(Collision2D col)
+        {
+            if (col.gameObject.CompareTag(GameObjectTags.Character))
+                _tossComponent.Fire();
+        }
+
+        private void OnDied() => 
+            _rigidbody.simulated = false;
     }
 }
--- src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Snake/SnakeView.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Snake/SnakeView.cs
@@ -1,9 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game
 {
     public class SnakeView : MonoBehaviour
     {
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+        private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
+        private static readonly int Death = Animator.StringToHash("Death");
+
+        [SerializeField] private GameObject _snake;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private TakeDamageColorComponent _damageComponent;
         
+        private HealthComponent _healthComponent;
+        private GroundedComponent _groundedComponent;
+        private MoveComponent _moveComponent;
+
+        private void Awake()
+        {
+            _healthComponent = _snake.GetComponent<HealthComponent>();
+            _groundedComponent = _snake.GetComponent<GroundedComponent>();
+            _moveComponent = _snake.GetComponent<MoveComponent>();
+
+            _healthComponent.OnHealthChanged += OnHealthChanged;
+            _healthComponent.OnDied += OnDied;
+            _groundedComponent.OnGrounded += OnGrounded;
+        }
+
+        private void OnDestroy()
+        {
+            _healthComponent.OnHealthChanged -= OnHealthChanged;
+            _healthComponent.OnDied -= OnDied;
+            _groundedComponent.OnGrounded -= OnGrounded;
+        }
+
+        private void Update() => 
+            OnMove(_moveComponent.IsMoving);
+
+        private void OnGrounded(bool isGrounded) => 
+            _animator.SetBool(IsGrounded, isGrounded);
+        
+        private void OnMove(bool isMoving) => 
+            _animator.SetBool(IsMoving, isMoving);
+
+        private void OnHealthChanged(float health)
+        {
+            if(health > 0)
+                _damageComponent.TakeDamage();
+        }
+
+        private void OnDied() => 
+            _animator.SetTrigger(Death);
     }
 }
