5943bfa369891cc331167b9dce1b5f689b00de52
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-04-28 00:57:59 +0300

    Заново собрал обезьяну

--- src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Monkey/Monkey.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Monkey/Monkey.cs
@@ -2,8 +2,97 @@
 
 namespace Game
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(HealthComponent))]
+    [RequireComponent(typeof(ExtraGravityComponent))]
+    [RequireComponent(typeof(GroundedComponent))]
+    [RequireComponent(typeof(JumpComponent))]
+    [RequireComponent(typeof(LookComponent))]
+    [RequireComponent(typeof(CollisionComponent))]
     public class Monkey : MonoBehaviour
     {
+        [SerializeField] private GameObject _pushAttack;
+        [SerializeField] private GameObject _target;
+        [SerializeField, Min(0)] private float _damage;
         
+        private Rigidbody2D _rigidbody;
+        private HealthComponent _healthComponent;
+        private ExtraGravityComponent _extraGravityComponent;
+        private JumpComponent _jumpComponent;
+        private LookComponent _lookComponent;
+        private CollisionComponent _collisionComponent;
+        private FireComponent _pushComponent;
+        private TriggerComponent _characterTriggerComponent;
+        private GroundedComponent _groundedComponent;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _healthComponent = GetComponent<HealthComponent>();
+            _extraGravityComponent = GetComponent<ExtraGravityComponent>();
+            _groundedComponent = GetComponent<GroundedComponent>();
+            _jumpComponent = GetComponent<JumpComponent>();
+            _lookComponent = GetComponent<LookComponent>();
+            _collisionComponent = GetComponent<CollisionComponent>();
+            _pushComponent = _pushAttack.GetComponent<FireComponent>();
+            _characterTriggerComponent = GetComponentInChildren<TriggerComponent>();
+
+            _pushComponent.SetCondition(() => _healthComponent.IsAlive);
+            _jumpComponent.SetCondition(() => _healthComponent.IsAlive &&
+                                              _groundedComponent.IsGrounded);
+            
+            _healthComponent.OnDied += OnDied;
+            _collisionComponent.OnEntered += OnCollisionEntered;
+
+            _characterTriggerComponent.OnEntered += OnCharacterEntered;
+            _characterTriggerComponent.OnExited += OnCharacterExited;
+
+            _groundedComponent.OnGrounded += OnGrounded;
+        }
+
+        private void OnDestroy()
+        {
+            _healthComponent.OnDied -= OnDied;
+            _collisionComponent.OnEntered -= OnCollisionEntered;
+
+            _characterTriggerComponent.OnEntered -= OnCharacterEntered;
+            _characterTriggerComponent.OnExited -= OnCharacterExited;
+            
+            _groundedComponent.OnGrounded -= OnGrounded;
+        }
+
+        private void Update()
+        {
+            _extraGravityComponent.enabled = _rigidbody.linearVelocityY < 0;
+            
+            _jumpComponent.Jump();
+            
+            if(_target)
+                _lookComponent.Look(_target.transform);
+        }
+
+        private void OnGrounded(bool _) => 
+            _pushComponent.Fire();
+
+        private void OnCharacterEntered(Collider2D col)
+        {
+            if(col.gameObject.layer == LayerMask.NameToLayer(GameObjectTags.Character))
+                _target = col.gameObject;
+        }
+
+        private void OnCharacterExited(Collider2D col)
+        {
+            if(col.gameObject.layer == LayerMask.NameToLayer(GameObjectTags.Character))
+                _target = null;
+        }
+
+        private void OnCollisionEntered(Collision2D col)
+        {
+            if(_damage > 0 && col.gameObject.TryGetComponent(out HealthComponent health))
+                health.TakeDamage(_damage);
+        }
+
+        private void OnDied() => 
+            _rigidbody.simulated = false;
     }
 }
--- src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Monkey/MonkeyView.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/GameObjects/Content/Monkey/MonkeyView.cs
@@ -4,6 +4,51 @@ namespace Game
 {
     public class MonkeyView : MonoBehaviour
     {
+        private static readonly int Jump = Animator.StringToHash("Jump");
+        private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
+        private static readonly int Death = Animator.StringToHash("Death");
         
+        [SerializeField] private GameObject _monkey;
+        [SerializeField] private TakeDamageColorComponent _damageComponent;
+        [SerializeField] private Animator _animator;
+        
+        private HealthComponent _healthComponent;
+        private GroundedComponent _groundedComponent;
+        private JumpComponent _jumpComponent;
+
+        private void Awake()
+        {
+            _healthComponent = _monkey.GetComponent<HealthComponent>();
+            _groundedComponent = _monkey.GetComponent<GroundedComponent>();
+            _jumpComponent = _monkey.GetComponent<JumpComponent>();
+
+            _healthComponent.OnHealthChanged += OnHealthChanged;
+            _healthComponent.OnDied += OnDied;
+            _groundedComponent.OnGrounded += OnGrounded;
+            _jumpComponent.OnJump += OnJump;
+        }
+
+        private void OnDestroy()
+        {
+            _healthComponent.OnHealthChanged -= OnHealthChanged;
+            _healthComponent.OnDied -= OnDied;
+            _groundedComponent.OnGrounded -= OnGrounded;
+            _jumpComponent.OnJump -= OnJump;
+        }
+
+        private void OnGrounded(bool isGrounded) => 
+            _animator.SetBool(IsGrounded, isGrounded);
+
+        private void OnJump() => 
+            _animator.SetTrigger(Jump);
+
+        private void OnHealthChanged(float health)
+        {
+            if(health > 0)
+                _damageComponent.TakeDamage();
+        }
+
+        private void OnDied() => 
+            _animator.SetTrigger(Death);
     }
 }
