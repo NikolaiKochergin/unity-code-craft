cc9e65bc3b973632105814a12558551aef7dc45f
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-05-01 01:08:14 +0300

    забиндил UI

---
+++ src/UnityCodeCraft/Assets/Game/Scripts/UI/Game/GameUIAPI.cs
@@ -0,0 +1,10 @@
+﻿using Atomic.Entities;
+using Game.Modules;
+
+namespace Game.UI
+{
+    public static class GameUIAPI
+    {
+        public static ValueKey<IGameUI, Joystick> Joystick = new(nameof(Joystick));
+    }
+}
---
+++ src/UnityCodeCraft/Assets/Game/Scripts/UI/Game/GameUIInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Atomic.Entities;
+using Game.Modules;
+using TMPro;
+using UnityEngine;
+
+namespace Game.UI
+{
+    public sealed class GameUIInstaller : SceneEntityInstaller<IGameUI>
+    {
+        [SerializeField] private HealthScreenView _healthScreenView;
+        [SerializeField] private Joystick _moveJoystick;
+        [SerializeField] private Joystick _attackJoystick;
+        [SerializeField] private StatView _healthView;
+        [SerializeField] private StatView _ammoView;
+        [SerializeField] private TMP_Text _killsView;
+        
+        public override void Install(IGameUI ui)
+        {
+            
+        }
+    }
+}
