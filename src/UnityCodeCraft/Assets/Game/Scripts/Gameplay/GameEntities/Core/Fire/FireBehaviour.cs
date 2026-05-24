49cc61431ba33a54dacc62a2fd506fb302db7759
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-05-25 00:40:41 +0300

    отредактировал механику задержки выстрела

--- src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameEntities/Core/Fire/FireBehaviour.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameEntities/Core/Fire/FireBehaviour.cs
@@ -13,13 +13,19 @@ namespace Game.Gameplay
         {
             _request = entity.GetValue(GameEntityAPI.FireRequest);
             _command = entity.GetValue(GameEntityAPI.FireCommand);
+
             entity.TryGetValue(GameEntityAPI.FireDelay, out _delay);
         }
 
         public void FixedTick(IGameEntity entity, float deltaTime)
         {
-            if(_request.Consume())
-                _command.Invoke();
+            if (_request.Consume())
+                if(_delay == null || _delay.IsCompleted())
+                    _command.Invoke();
+                else
+                    _delay.Tick(deltaTime);
+            else if(_delay != null && _delay.GetProgress() < 1f)
+                _delay.ResetTime();
         }
     }
 }
