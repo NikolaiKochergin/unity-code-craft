e4b2f57c2725e1bf88af1ace3f766dfddcfeb2b4
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-05-24 22:43:33 +0300

    упростил зомби триггер

---
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameEntities/Content/Zombie/ZombieTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Atomic.Entities;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class ZombieTrigger : MonoBehaviour
+    {
+        [SerializeField] private GameEntity[] _zombies;
+        
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.TryGetComponent(out IGameEntity entity) && entity.HasTag(GameEntityAPI.CharacterTag))
+                foreach (GameEntity zombie in _zombies)
+                    zombie.GetValue(GameEntityAPI.Target).Value = entity;
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            if(col.TryGetComponent(out IGameEntity entity) && entity.HasTag(GameEntityAPI.CharacterTag))
+                foreach (GameEntity zombie in _zombies)
+                    zombie.GetValue(GameEntityAPI.Target).Value = null;
+        }
+    }
+}
