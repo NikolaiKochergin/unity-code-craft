587d70e56dfbff54fee2dbd8ed1b2af3603d20f2
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-05-29 00:16:21 +0300

    добавил комманду стоп

---
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameSystem/CommandQueue/StopCommand.cs
@@ -0,0 +1,18 @@
+﻿using Modules.AI;
+using UnityEngine;
+
+namespace Game
+{
+    public class StopCommand : IUnitCommand
+    {
+        public void Start(Blackboard blackboard)
+        {
+            blackboard.GetValue(BlackboardAPI.CommandQueue).Reset();
+            GameObject basePoint = blackboard.GetValue(BlackboardAPI.BasePoint);
+            GameObject character = blackboard.GetValue(BlackboardAPI.Character);
+            basePoint.transform.position = character.transform.position;
+        }
+
+        public void Stop(Blackboard blackboard) { }
+    }
+}
--- src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameSystem/Handlers/StopCommandHandler.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameSystem/Handlers/StopCommandHandler.cs
@@ -1,3 +1,5 @@
+using Game;
+using Modules.AI;
 using UnityEngine;
 
 namespace SampleGame
@@ -12,12 +14,30 @@ namespace SampleGame
         
         [SerializeField]
         private InputHandler _next;
+
+        private UnitCommandQueue _commandQueue;
+
+        private void Start()
+        {
+            Blackboard blackboard = _character.GetComponentInChildren<Blackboard>();
+            
+            if (blackboard == null) 
+                Debug.LogError("No Blackboard component found on " + _character);
+            
+            _commandQueue = blackboard?.GetValue(BlackboardAPI.CommandQueue);
+            
+            if (_commandQueue == null)
+                Debug.LogError("CommandQueue doesn't exist on " + _character);
+        }
         
         public override void Handle(ref InputContext context)
         {
             if (Input.GetKeyDown(_keyCode))
             {
-                // TODO: Stop
+                if (!context.enqueueCommand)
+                    _commandQueue.Reset();
+                
+                _commandQueue.Add(new StopCommand());
             }
             else if (_next)
                 _next.Handle(ref context);
