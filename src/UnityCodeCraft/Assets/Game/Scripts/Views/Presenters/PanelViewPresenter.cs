7c4ddf89075eadced4896a70926333fe8c00fdf9
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-08-24 00:10:04 +0300

    работа над системой дохода игрока

--- src/UnityCodeCraft/Assets/Game/Scripts/Views/Presenters/PanelViewPresenter.cs
+++ src/UnityCodeCraft/Assets/Game/Scripts/Views/Presenters/PanelViewPresenter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using SampleGame;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
@@ -14,6 +15,7 @@ namespace Game
         private readonly List<UnitCardVewPresenter> _presenters = new();
         private World _world;
         private Entity _playerEntity;
+        private EntityManager _entityManager;
 
         private void Start()
         {
@@ -28,6 +30,53 @@ namespace Game
         {
             foreach (UnitCardVewPresenter presenter in _presenters) 
                 presenter.Update();
+            
+            if (_entityManager == default)
+            {
+                World world = World.DefaultGameObjectInjectionWorld;
+
+                if (world == null)
+                    return;
+
+                _entityManager = world.EntityManager;
+            }
+
+            if (_playerEntity == Entity.Null)
+            {
+                FindPlayerEntity();
+
+                if (_playerEntity == Entity.Null)
+                    return;
+            }
+
+            if (!_entityManager.Exists(_playerEntity))
+            {
+                _playerEntity = Entity.Null;
+                return;
+            }
+            
+            _panelView.EnergyAmountText = _entityManager.GetComponentData<Money>(_playerEntity).Value.ToString();
+        }
+
+        private void FindPlayerEntity()
+        {
+            EntityQuery query = _entityManager.CreateEntityQuery(
+                ComponentType.ReadOnly<Player>(),
+                ComponentType.ReadOnly<Money>(),
+                ComponentType.ReadOnly<Team>()
+            );
+
+            using NativeArray<Entity> entities = query.ToEntityArray(Allocator.Temp);
+
+            foreach (Entity entity in entities)
+            {
+                Team team = _entityManager.GetComponentData<Team>(entity);
+                if (team.Value == _team)
+                {
+                    _playerEntity = entity;
+                    return;
+                }
+            }
         }
     }
 }
