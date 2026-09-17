57f4f7ceb9f306b75176bd6a9ecafd601b30a93e
Nikolai Kochergin <Kocherginizer@gmail.com>
2026-09-18 00:55:43 +0300

    добавил базовое передвижение игрока. Но нужно разобраться и починить стейт ауторити передвижения

---
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/GameObjects/Core/Move/MoveComponent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Fusion;
+using UnityEngine;
+
+namespace Game
+{
+    public sealed class MoveComponent : NetworkBehaviour
+    {
+        public interface ICondition
+        {
+            public bool IsMet();
+        }
+
+        [SerializeField] private float _moveSpeed = 5f;
+        [SerializeField] private float _angularSpeed = 720f;
+        [SerializeField] private bool _lookRotation = true;
+        
+        private ICondition _condition;
+        
+        [Networked, OnChangedRender(nameof(MoveDirectionChanged))]
+        public Vector3 MoveDirection { get; private set; }
+
+        public event Action OnStateChanged;
+
+        public void SetCondition(ICondition condition) => 
+            _condition = condition;
+
+        public void Move(Vector3 direction)
+        {
+            MoveDirection = _condition == null || _condition.IsMet() ? direction : Vector3.zero;
+            
+            if(MoveDirection == Vector3.zero)
+                return;
+
+            float deltaTime = Time.fixedDeltaTime;
+            UpdatePosition(MoveDirection, deltaTime);
+
+            if (_lookRotation)
+                UpdateRotation(MoveDirection, deltaTime);
+        }
+
+        public void Stop()
+        {
+            MoveDirection = Vector3.zero;
+        }
+
+        private void UpdatePosition(Vector3 direction, float deltaTime) => 
+            transform.position += direction * deltaTime * _moveSpeed;
+
+        private void UpdateRotation(Vector3 direction, float deltaTime)
+        {
+            Quaternion current = transform.rotation;
+            Quaternion target = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(current, target, _angularSpeed * deltaTime);
+        }
+
+        private void MoveDirectionChanged()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
---
+++ src/UnityCodeCraft/Assets/Game/Scripts/Gameplay/PlayerContext/Core/PlayerInputController.cs
@@ -0,0 +1,57 @@
+﻿using Fusion;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+    public sealed class PlayerInputController : NetworkBehaviour
+    {
+        [Networked]
+        private NetworkButtons _previousButtons { get; set; }
+
+        private PlayerCharacterProvider _characterProvider;
+
+
+        [Inject]
+        public void Construct(PlayerCharacterProvider characterProvider)
+        {
+            _characterProvider = characterProvider;
+        }
+
+        // TODO: дописать передвижение и разобраться что не так со стейт ауторити
+        
+        public override void FixedUpdateNetwork()
+        {
+            
+            
+            
+            NetworkObject character = _characterProvider.Character;
+            if(character == null)
+                return;
+            
+            if(GetInput(out InputData inputData))
+            {
+                NetworkButtons inputButtons = inputData.buttons;
+                ProcessMove(character, inputData.moveDirection);
+                
+                _previousButtons = inputButtons;
+            }
+            else
+            {
+                StopMove(character);
+            }
+        }
+
+        private void ProcessMove(NetworkObject character, Vector2 inputDirection)
+        {
+            MoveComponent moveComponent = character.GetBehaviour<MoveComponent>();
+            Vector3 moveDirection = new(inputDirection.x, 0, inputDirection.y);
+            moveComponent.Move(moveDirection);
+        }
+
+        private void StopMove(NetworkObject character)
+        {
+            character.GetBehaviour<MoveComponent>().Stop();
+        }
+    }
+}
